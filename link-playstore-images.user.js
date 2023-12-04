7abddeac8dddf4a830b615670201818cffad2806
fxolan <56937889+fxolan@users.noreply.github.com>
2023-12-03 21:22:34 -0400

    Update link-playstore-images.user.js
    simplify

--- link-playstore-images.user.js
+++ link-playstore-images.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Link Images on Playstore
 // @namespace    https://github.com/fxolan/userscripts
-// @version      1.0
+// @version      1.0.1
 // @description  Open full page link of image when clicked on Google Play app page
 // @author       Abdurazaaq Mohammed
 // @match        *://play.google.com/store/apps/*
@@ -12,20 +12,13 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+  'use strict';
 
-    // Select all images on the page
-    var images = document.getElementsByTagName('img');
+  const images = document.getElementsByTagName('img');
 
-    // Add a click event listener to each image
-    for (var i = 0; i < images.length; i++) {
-        images[i].addEventListener('click', function(e) {
-
-            // Get the image URL and remove size parameters to get the full size image
-            var noSizeParams = this.src.split('=')[0];
-            var fullSizeImageUrl = noSizeParams + "=w2560-h1440-rw";
-            // Open the full size image in a new tab
-            window.open(fullSizeImageUrl);
-        });
-    }
+  for (var i = 0; i < images.length; i++) {
+      images[i].addEventListener('click', function(e) {
+          window.open(this.src.split('=')[0] + "=w2560-h1440-rw");
+      });
+  }
 })();
