9f077bf8f5f02314adcc36a7f51c097343af1d05
fxolan <56937889+fxolan@users.noreply.github.com>
2023-12-08 14:13:43 -0400

    Update yt-download-via-savefrom.user.js
    actually the links are already sorted in order of quality
    close the tab after a few seconds. if your browser opens the download in an inbuilt video/audio player this should not close it.
    fix choosing audio/video

--- yt-download-via-savefrom.user.js
+++ yt-download-via-savefrom.user.js
@@ -1,16 +1,20 @@
 // ==UserScript==
 // @name        Download video/audio from YouTube quickly via SaveFrom
 // @namespace   https://github.com/fxolan/userscripts
-// @version     1.0
+// @version     1.0.1
 // @description Adds buttons in userscript manager on YouTube videos to go to SaveFrom and get video or audio, and automatically selects and downloads the highest quality video/audio on SaveFrom links
 // @match       https://en.savefrom.net/*
 // @match       https://www.youtube.com/*
 // @exclude     https://www.youtube.com/feed/*
 // @exclude     https://www.youtube.com/channel/*
 // @grant       GM_registerMenuCommand
+// @grant       GM_setValue
+// @grant       GM_getValue
 // @homepage    https://github.com/fxolan/userscripts
 // @license     The Unlicense
 // @supportURL  https://github.com/fxolan/userscripts/issues
+// @downloadURL https://update.greasyfork.org/scripts/481649/Download%20videoaudio%20from%20YouTube%20quickly%20via%20SaveFrom.user.js
+// @updateURL https://update.greasyfork.org/scripts/481649/Download%20videoaudio%20from%20YouTube%20quickly%20via%20SaveFrom.meta.js
 // ==/UserScript==
 
 (function() {
@@ -28,41 +32,30 @@
   function getHighestQuality() {
     // Get all the video download links on the page
     const links = document.querySelectorAll('.main > div.link-group > .ga_track_events.subname.link-download.link');
-
-    let highestQuality = 0;
-    let highestQualityLink = null;
+    const format = GM_getValue('format') ? 'MP4' : 'Audio';
 
     // Iterate through the video download links
     for (let i = 0; i < links.length; i++) {
       const link = links[i];
-      const words = link.innerText.split(" ");
-      const lastWord = words.pop();
-      if(localStorage.getItem('format')) {
-        if(link.innerText.startsWith('MP4')) {
-          if (parseInt(lastWord) > highestQuality) {
-            highestQuality = parseInt(lastWord);
-            highestQualityLink = link;
-           }
-        }
-      }
-      else {
-        if(link.innerText.startsWith('Audio')) {
-          if (parseInt(lastWord) > highestQuality) {
-            highestQuality = parseInt(lastWord);
-            highestQualityLink = link;
-          }
-        }
+      if(link.innerText.startsWith(format)) { //actually the links are already sorted in order of quality
+        link.click();
+        break;
       }
     }
-    highestQualityLink.click();
+
+    set = setInterval(function() { //close the tab after a few seconds. if your browser opens the download in an inbuilt video/audio player this should not close it.
+      window.close();
+      clearInterval(set);
+    }, 2000);
   }
 
   function dlV() {
+    GM_setValue('format', true);
     window.open(saveF + url, '_blank').focus();
   }
 
   function dlA() {
-    localStorage.setItem('format', 'false');
+    GM_setValue('format', false);
     window.open(saveF + url, '_blank').focus();
   }
 
@@ -75,9 +68,7 @@
       }
     }, 200);
   } else {
-    localStorage.setItem('format', true);
     GM_registerMenuCommand('Download Video', dlV);
-	  GM_registerMenuCommand('Download Audio', dlA);
+    GM_registerMenuCommand('Download Audio', dlA);
   }
-
 })();
