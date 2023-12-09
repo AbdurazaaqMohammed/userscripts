a870739b36f4d9572f760504f9cdd0401ed73353
fxolan <56937889+fxolan@users.noreply.github.com>
2023-12-09 15:31:55 -0400

    Update upload-from-ezgif-to-mobilism.user.js

--- upload-from-ezgif-to-mobilism.user.js
+++ upload-from-ezgif-to-mobilism.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Upload image from Ezgif to Mobilim Images
+// @name         Upload image from Ezgif to Mobilism Images
 // @namespace    https://github.com/fxolan
 // @version      1.0
-// @description  Adds a button to upload the converted image from ezgif to Mobilism
+// @description  Upload the converted image from ezgif to Mobilism and copies the image link after converting it.
 // @author       Abdurazaaq Mohammed
 // @match        https://ezgif.com/*/*.*
 // @match        https://images.mobilism.org/
@@ -20,27 +20,11 @@
   const url = window.location.href;
   const mobilism = 'https://images.mobilism.org/';
 
-  function addButton() {
-    const container = document.createElement('div');
-    container.style.position = 'fixed';
-    container.style.top = '10px';
-    container.style.left = '10px';
-
-    const button = document.createElement('button');
-    button.innerText = 'Upload To Mobilism Images';
-    button.addEventListener('click', getAndGoToMobilism);
-
-    container.appendChild(button);
-    document.body.appendChild(container);
-  }
-
   function getAndGoToMobilism() {
     var image = document.getElementById('output').getElementsByTagName('img')[0];
     if (image) {
       GM_setValue('u', image.src);
-      setTimeout(function() {
-        window.location.href = mobilism;
-      }, 500);
+      window.location.href = mobilism;
     } else {
       alert('Image not found!');
     }
@@ -61,7 +45,7 @@
   if (url.endsWith(mobilism)) {
     window.addEventListener('load', uploadImg);
   } else if (url.startsWith('https://ezgif')) {
-   window.addEventListener('load', addButton);
+   window.addEventListener('load', getAndGoToMobilism);
   } else {
     console.log(GM_getValue('u'));
     window.addEventListener('load', copyImageLink);
