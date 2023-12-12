ffb7d7560ce85d7806c0ff09592b7019227b8b2d
fxolan <56937889+fxolan@users.noreply.github.com>
2023-12-12 10:22:21 -0400

    Update format-playstore-info-for-mobilism.user.js
    make it less stupid (it is still stupid)

--- format-playstore-info-for-mobilism.user.js
+++ format-playstore-info-for-mobilism.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy information of apps from Play Store to post on Mobilism
 // @namespace    https://github.com/fxolan/userscripts
-// @version      1.0
+// @version      1.0.1
 // @description  Copy information of apps from Play Store and format it to post on Mobilism
 // @author       Abdurazaaq Mohammed
 // @match        https://play.google.com/store/apps/*
@@ -25,12 +25,23 @@
       const requiredVersion = document.querySelector("#yDmH0d > div:nth-child(30) > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi > div:nth-child(5) > div:nth-child(5) > div.reAt0").innerText;
       const version = document.querySelector("#yDmH0d > div:nth-child(30) > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi > div:nth-child(5) > div:nth-child(2) > div.reAt0").innerText;
       const desc = document.querySelector("#yDmH0d > div:nth-child(30) > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi > div:nth-child(1)").innerText;
-      const changelog = document.querySelector("#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(3) > section > div > div").innerText;
+      var changelog = document.querySelector("#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(3) > section > div > div").innerText;
+      if (changelog.startsWith('This app may share')) changelog = document.querySelector("#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(5) > section > div > div").innerText;
       const firstLine = desc.split('\n')[0];
-      const remainingLines = desc.split('\n').slice(1).join('\n');
-      const bruh = '[b]' + title + ' v' + version + '[/b]' + '\n[u]Requirements:[/u] ' + requiredVersion + '\n[u]Overview:[/u] ' + firstLine + '\n[img]https://images.mobilism.org/?dm=NUG2WDGF[/img]\n[break]\n' + remainingLines + "\n\n[u]What's New:[/u]\n" + changelog + '\n\n[b]This app has no advertisements[/b]\n\n[u]More Info:[/u]\n[code]' + window.location.href + '[/code]\n[u]Download Instructions:[/u]\nUniversal\n\nMirror:\n\narmeabi-v7a\n\nMirror:\n\narm64-v8a\n\nMirror:\n\nx86\n\nMirror:\n\nx86_64\n\nMirror:\n';
-      if (bruh) {
-        GM_setClipboard(bruh);
+      const postDesc = desc.split('\n').slice(1).join('\n');
+      const postTitle = '[b]' + title + ' v' + version + '[/b]';
+      const postRequirements = '[u]Requirements:[/u] ' + requiredVersion;
+      const postOverview = '[u]Overview:[/u] ' + firstLine;
+      const img = 'YOU STILL HAVE TO PUT THE IMAGE\n[break]';
+      const modInfo = ''; //put mod info if you want
+      const postChangelog = "[u]What's New:[/u]\n" + changelog;
+      const adsField = '[b]This app has no advertisements[/b]';
+      const playstoreLink = '[u]More Info:[/u]\n[code]' + window.location.href + '[/code]';
+      const dlLinks = '[u]Download Instructions:[/u]\nUniversal\n\nMirror:\n\narmeabi-v7a\n\nMirror:\n\narm64-v8a\n\nMirror:\n\nx86\n\nMirror:\n\nx86_64\n\nMirror:\n';
+      const msg = 'Trouble downloading? Read [url=https://forum.mobilism.org/viewtopic.php?f=19&t=649944][b]This[/b][/url].';
+      const fullInfo = postTitle + '\n' + postRequirements + '\n' + postOverview +'\n\n' + img + '\n' + postDesc + '\n' + modInfo + '\n\n' + postChangelog + '\n\n' + adsField + '\n\n' + playstoreLink + '\n' + dlLinks + '\n' + msg;
+      if (fullInfo) {
+        GM_setClipboard(fullInfo);
         alert('Copied info to clipboard.');
       } else {
         alert('Error; try increase the loading time in the script');
