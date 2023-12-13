b80cb301548af37ce8112cf26da56a5bcdf992f9
fxolan <56937889+fxolan@users.noreply.github.com>
2023-12-13 19:40:39 -0400

    Create fdroid-open-img-on-click.user.js

---
+++ fdroid-open-img-on-click.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Open F-Droid Images in New Tab
+// @namespace    https://github.com/fxolan/userscripts
+// @author       Abdurazaaq Mohammed
+// @version      1.9
+// @description  Open F-Droid images in a new tab when clicked
+// @match        https://f-droid.org/*/packages/*
+// @run-at       document-start
+// @grant        none
+// @license      The Unlicense
+// @homepage     https://github.com/fxolan/userscripts
+// @supportURL   https://github.com/fxolan/userscripts/issues
+// ==/UserScript==
+
+(function() {
+  'use strict';
+    const images = document.querySelectorAll('.js_slide.screenshot');
+
+    for (let i = 0; i < images.length; i++) {
+      images[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        window.open(this.querySelector('img').src, '_blank');
+      });
+    }
+})();
