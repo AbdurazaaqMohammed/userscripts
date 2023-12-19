5d43d07bc0f38bbcf34880557fc9f8012ee30549
fxolan <56937889+fxolan@users.noreply.github.com>
2023-12-19 13:01:45 -0400

    Update format-playstore-info-for-mobilism.user.js
    fixed app version and requirements were mixed up
    use innertext for changelog to fix spacing issues

--- format-playstore-info-for-mobilism.user.js
+++ format-playstore-info-for-mobilism.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy information of apps from Play Store to post on Mobilism
 // @namespace    https://github.com/fxolan/userscripts
-// @version      1.0.4
+// @version      1.0.4.1
 // @description  Copy information of apps from Play Store and format it to post on Mobilism
 // @author       Abdurazaaq Mohammed
 // @match        https://play.google.com/store/apps/*
@@ -22,10 +22,10 @@
 
     setTimeout ( function () {
       const title = document.querySelector('h1').textContent;
-      var appVersion = document.querySelector("#yDmH0d > div.VfPpkd-Sx9Kwc.cC1eCc.UDxLd.PzCPDd.HQdjr.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi > div:nth-child(3) > div:nth-child(3) > div.reAt0").textContent;
-      var requiredVersion = document.querySelector('#yDmH0d > div.VfPpkd-Sx9Kwc.cC1eCc.UDxLd.PzCPDd.HQdjr.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi > div:nth-child(3) > div:nth-child(1) > div.reAt0').textContent;
+      var appVersion = document.querySelector("#yDmH0d > div.VfPpkd-Sx9Kwc.cC1eCc.UDxLd.PzCPDd.HQdjr.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi > div:nth-child(3) > div:nth-child(1) > div.reAt0").textContent;
+      var requiredVersion = document.querySelector('#yDmH0d > div.VfPpkd-Sx9Kwc.cC1eCc.UDxLd.PzCPDd.HQdjr.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi > div:nth-child(3) > div:nth-child(3) > div.reAt0').textContent;
       if (requiredVersion.startsWith('Varies with device')) {
-        for (let i=5; i<5+6; i++) { // check version and requirements from your linked google devices. replace '5+6' with 5 + number of devices linked to your google account. note this takes the first existing version from your compatible devices. it may be outdated, you might want to check it from apkpure.
+        for (let i=5; i<5+6; i++) { // check version and requirements for your linked google devices
           requiredVersion = "#yDmH0d > div.VfPpkd-Sx9Kwc.cC1eCc.UDxLd.PzCPDd.HQdjr.VfPpkd-Sx9Kwc-OWXEXe-FNFY6c > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi > div:nth-child(" + i + ") > div:nth-child(5) > div.reAt0";
           if (document.querySelector(requiredVersion)) {
             appVersion =  document.querySelector(requiredVersion.replace('div:nth-child(5) > div.reAt0', 'div:nth-child(2) > div.reAt0')).textContent;
@@ -35,8 +35,8 @@
         }
       }
       const desc = document.querySelector("#yDmH0d > div:nth-child(30) > div.VfPpkd-wzTsW > div > div > div > div > div.fysCi > div:nth-child(1)").innerText.replace(/\p{Emoji}/gu, '');
-      var changelog = document.querySelector("#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(3) > section > div > div") ? document.querySelector("#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(3) > section > div > div").textContent : 'No changelog';
-      if (changelog.startsWith('This app may share') || changelog.startsWith('No data shared')) changelog = document.querySelector("#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(5) > section > div > div").textContent;
+      var changelog = document.querySelector("#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(3) > section > div > div") ? document.querySelector("#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(3) > section > div > div").innerText : 'No changelog';
+      if (changelog.startsWith('This app may share') || changelog.startsWith('No data shared')) changelog = document.querySelector("#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(5) > section > div > div").innerText;
       changelog = changelog.replace(/\p{Emoji}/gu, '');
       const firstLine = desc.split('\n')[0];
       const postDesc = desc.split('\n').slice(1).join('\n');
