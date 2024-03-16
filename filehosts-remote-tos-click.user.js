37ec2183285975dc9e582b3f84c6ddfd702431b8
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-03-16 15:18:43 -0400

    Update filehosts-remote-tos-click.user.js
    auto focus the input field

--- filehosts-remote-tos-click.user.js
+++ filehosts-remote-tos-click.user.js
@@ -1,14 +1,18 @@
 // ==UserScript==
 // @name         Auto Remote Upload + ToS
 // @namespace    https://github.com/AbdurazaaqMohammed
-// @version      1.0
+// @version      1.1
 // @description  Automatically selects remote URL upload and ticks the TOS box on file hosting sites often used on Mobilism
 // @author       Abdurazaaq Mohammed
-// @match        *://userupload.*/?op=upload_form
-// @match        *://uploadrar.com/
-// @match        *://devuploads.com/upload
-// @match        *://dropgalaxy.*/
-// @match        *://dgdrive.xyz/
+// @match        https://userupload.net/?op=upload_form
+// @match        https://userupload.in/?op=upload_form
+// @match        https://uploadrar.com/
+// @match        https://devuploads.com/upload
+// @match        https://dropgalaxy.vip/
+// @match        https://dropgalaxy.co/
+// @match        https://dropgalaxy.com/
+// @match        https://dgdrive.xyz/
+// @match        https://apkadmin.com/
 // @grant        none
 // @homepage     https://github.com/AbdurazaaqMohammed/userscripts
 // @license      The Unlicense
@@ -19,13 +23,19 @@
   'use strict';
 
   const url = window.location.href;
+  const inputField = document.querySelector("textarea");
+  setTimeout(function() { // auto focus the input field
+      if(inputField && document.activeElement !== inputField){
+        inputField.focus();
+      }
+    }, 500);
 
   if (url.includes('dropgalaxy') || url.includes('dgdrive')) {
     document.querySelector("#tab-remote_upload > span").click();
   }
   else if (url.includes('userupload')) {
     document.querySelector("#select_url").click();
-    document.querySelector(".custom-control-label").click();//devuploads and uploadrar already enables TOS by default. I can't remember about DG
+    document.querySelector(".custom-control-label").click(); //devuploads and uploadrar already enable TOS by default. I can't remember about DG
   }
   else {
     const form = document.querySelector("#select_url");
