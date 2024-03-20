27ca2e12c3c1fc8c09056b17a4204891e9bfc0e1
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-03-20 15:17:01 -0400

    Update filegarden-black-background.user.js
    Set with JS to avoid setting selected files (for clarity)

--- filegarden-black-background.user.js
+++ filegarden-black-background.user.js
@@ -1,22 +1,23 @@
 // ==UserScript==
 // @name           File.Garden Black Background
 // @namespace      https://github.com/AbdurazaaqMohammed
-// @version        1.0
+// @version        1.1
 // @author         Abdurazaaq Mohammed
 // @description    Change background color to AMOLED black on File.Garden
 // @match          https://filegarden.com/*
 // @homepage       https://github.com/AbdurazaaqMohammed/userscripts
 // @license        The Unlicense
 // @supportURL     https://github.com/AbdurazaaqMohammed/userscripts/issues
-// @grant          GM_addStyle
+// @grant          none
 // ==/UserScript==
 
 (function() {
   'use strict';
 
-  GM_addStyle(`
-    div {
-      background-color: black !important;
-    }
-  `);
+  const divs = document.querySelectorAll('div');
+
+  divs.forEach(function(div) {
+    if (div.id !== '#items > a.item.typeFile.selected') div.style.backgroundColor = 'black';
+  });
+
 })();
