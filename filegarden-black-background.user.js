aff213e277e892c9cb7c28b5f76ef89870ef4bbb
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-03-20 20:31:59 -0400

    Update filegarden-black-background.user.js
    turns out that this does not change the background color of the selected files anyway

--- filegarden-black-background.user.js
+++ filegarden-black-background.user.js
@@ -1,23 +1,21 @@
 // ==UserScript==
 // @name           File.Garden Black Background
 // @namespace      https://github.com/AbdurazaaqMohammed
-// @version        1.1
+// @version        1.1.1
 // @author         Abdurazaaq Mohammed
 // @description    Change background color to AMOLED black on File.Garden
 // @match          https://filegarden.com/*
 // @homepage       https://github.com/AbdurazaaqMohammed/userscripts
 // @license        The Unlicense
 // @supportURL     https://github.com/AbdurazaaqMohammed/userscripts/issues
-// @grant          none
+// @grant          GM_addStyle
 // ==/UserScript==
 
 (function() {
   'use strict';
 
-  const divs = document.querySelectorAll('div');
-
-  divs.forEach(function(div) {
-    if (div.id !== '#items > a.item.typeFile.selected') div.style.backgroundColor = 'black';
+  document.querySelectorAll('div').forEach(function(div) {
+    div.style.backgroundColor = 'black';
   });
 
 })();
