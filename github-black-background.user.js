b04e7df49557322ec7a191b628fb495158f9485e
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-06-27 13:31:23 -0400

    Create github-black-background.user.js

---
+++ github-black-background.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name           GitHub Black Background
+// @namespace      https://github.com/AbdurazaaqMohammed
+// @version        1.0
+// @author         Abdurazaaq Mohammed
+// @description    Change background color to AMOLED black on GitHub
+// @match          https://github.com/*
+// @homepage       https://github.com/AbdurazaaqMohammed/userscripts
+// @license        The Unlicense
+// @supportURL     https://github.com/AbdurazaaqMohammed/userscripts/issues
+// @run-at         document-start
+// @grant          GM_addStyle
+// ==/UserScript==
+(function() {
+	'use strict';
+	try {
+		GM_addStyle('div:not(button div) { background-color: black !important; }');
+	} catch (e) {
+		document.head.appendChild(document.createElement('style')).innerHTML = 'div:not(button div) { background-color: black !important; }';
+	}
+})();
