ada27f9de0e474cd5cc9d853c231510d20c5fc98
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-07-02 12:51:12 -0400

    Update old-reddit-black-backgroud.user.js
    fix videos

--- reddit/old-reddit-black-backgroud.user.js
+++ reddit/old-reddit-black-backgroud.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Old Reddit Black Background
 // @namespace      https://github.com/AbdurazaaqMohammed
-// @version        1.0
+// @version        1.1
 // @author         Abdurazaaq Mohammed
 // @description    Change background color to AMOLED black on Old Reddit
 // @match          https://old.reddit.com/*
@@ -10,13 +10,10 @@
 // @license        The Unlicense
 // @supportURL     https://github.com/AbdurazaaqMohammed/userscripts/issues
 // @run-at         document-start
-// @grant          GM_addStyle
+// @grant          none
 // ==/UserScript==
 (function() {
 	'use strict';
-	try {
-		GM_addStyle('div:not(button div) { background-color: black !important; }');
-	} catch (e) {
-		document.head.appendChild(document.createElement('style')).innerHTML = 'div:not(button div) { background-color: black !important; }';
-	}
+
+  document.head.appendChild(document.createElement('style')).innerHTML = 'div:not(button div):not(#video-' + window.location.href.match(/\/comments\/(\w+)\//)[1] +' div) { background-color: black !important; }';
 })();
