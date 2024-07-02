6307ab457dbe713ec4108094070a922bfe000ed8
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-07-02 18:05:05 -0400

    Update fix-old-reddit-images.user.js
    Use parameter on the website so no need for setValue or getValue

--- reddit/fix-old-reddit-images.user.js
+++ reddit/fix-old-reddit-images.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name        Fix image links on Old Reddit
-// @namespace   https://github.com/AbdurazaaqMohammed
-// @version     1.0
+// @namespace   https://github.com/abdurazaaqmohammed
+// @version     1.0.3
 // @description Fix opening redd.it image links on Old Reddit
 // @author      Abdurazaaq Mohammed
-// @homepage    https://github.com/AbdurazaaqMohammed/userscripts
+// @homepage    https://github.com/abdurazaaqmohammed/userscripts
 // @license     The Unlicense
-// @supportURL  https://github.com/AbdurazaaqMohammed/userscripts/issues
-// @match       https://old.reddit.com/*
-// @match       https://AbdurazaaqMohammed.github.io/website/imgviewer
-// @grant       GM_getValue
-// @grant       GM_setValue
+// @supportURL  https://github.com/abdurazaaqmohammed/userscripts/issues
+// @match       https://*.reddit.com/*
+// @exclude     https://www.reddit.com/*
+// @exclude     https://new.reddit.com/*
+// @grant       none
 // ==/UserScript==
 
 (function () {
@@ -18,27 +18,20 @@
   const url = window.location.href;
 
   function openImageViewer(imgLink) {
-    GM_setValue('img', imgLink);
     // Uncomment below and comment/delete the other line if you want to redirect to the image directly instead of opening in a new tab.
-    //window.location.href = 'https://AbdurazaaqMohammed.github.io/website/imgviewer';
-    window.open('https://AbdurazaaqMohammed.github.io/website/imgviewer');
+    //window.location.href = 'https://abdurazaaqmohammed.github.io/website/imgviewer?viewimg=' + imgLink;
+    window.open('https://abdurazaaqmohammed.github.io/website/imgviewer?viewimg=' + imgLink);
   }
 
   function fixImageLinks(mutationsList, observer) {
-    const imageLinks = document.querySelectorAll('a[href^="https://preview.redd.it"], a[href^="https://i.redd.it"]');
-    imageLinks.forEach(link => {
+    document.querySelectorAll('a[href^="https://preview.redd.it"], a[href^="https://i.redd.it"]').forEach(link => {
       const imgLink = link.href;
-      // Overwriting the href will break copying the image link
+      // Overwriting the href would break copying the image link, so remove the old href and set onClick to open the viewer instead.
       link.removeAttribute("href");
       link.addEventListener("click", function() { openImageViewer(imgLink); });
     });
   }
 
-  if (url.startsWith('https://old.reddit.com/')) {
-    const observer = new MutationObserver(fixImageLinks);
-    observer.observe(document.body, { subtree: true, childList: true });
-  } else {
-    document.getElementById('urlInput').value = GM_getValue('img');
-    document.getElementById('displayButton').click();
-  }
+  new MutationObserver(fixImageLinks).observe(document.body, { subtree: true, childList: true });
+
 })();
