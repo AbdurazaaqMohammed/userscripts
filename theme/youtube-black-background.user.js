9655453dc379ddf558cb678d8d8220275e7c8a12
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-07-06 09:27:15 -0400

    Update youtube-black-background.user.js
    fix on create new community post page

--- theme/youtube-black-background.user.js
+++ theme/youtube-black-background.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           YouTube Black Background
 // @namespace      https://github.com/AbdurazaaqMohammed
-// @version        1.5.4
+// @version        1.5.5
 // @author         Abdurazaaq Mohammed
 // @description    Change background color to AMOLED black on YouTube
 // @match          https://www.youtube.com/*
@@ -13,6 +13,6 @@
 // ==/UserScript==
 (function() {
 	'use strict';
-  document.head.appendChild(document.createElement('style')).innerHTML = 'div:not(button div):not(#container div):not(#dismissible div):not(#shorts-container div):not(.ytd-backstage-items.style-scope div):not(#contentWrapper div):not(#video-preview div):not(li div):not(#items *):not(#page-manager > ytd-browse > ytd-playlist-header-renderer div), ytd-playlist-panel-renderer div:not(ytd-playlist-panel-video-renderer div), yt-spec-touch-feedback-shape__fill div, #contentWrapper *:not(tp-yt-paper-item):not(tp-yt-paper-item *), #sections, #ytp-id-18, .ytd-rich-metadata-row-renderer *, body > ytd-app > ytd-popup-container > tp-yt-paper-dialog > ytd-voice-search-dialog-renderer { background-color: black !important; }';
+  document.head.appendChild(document.createElement('style')).innerHTML = 'div:not(button div):not(#container div):not(#dismissible div):not(#shorts-container div):not(.ytd-backstage-items.style-scope div):not(#contentWrapper div):not(#video-preview div):not(li div):not(#items *):not(#page-manager > ytd-browse > ytd-playlist-header-renderer div), ytd-playlist-panel-renderer div:not(ytd-playlist-panel-video-renderer div), yt-spec-touch-feedback-shape__fill div, #contentWrapper *:not(tp-yt-paper-item):not(tp-yt-paper-item *), #sections, #ytp-id-18, .ytd-rich-metadata-row-renderer *, body > ytd-app > ytd-popup-container > tp-yt-paper-dialog > ytd-voice-search-dialog-renderer, #backstage-post-dialog > ytd-backstage-post-dialog-renderer > div { background-color: black !important; }';
 
 })();
