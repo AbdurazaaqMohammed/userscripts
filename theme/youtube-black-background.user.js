732593c9dffb0c5ee7e83f6e1dcc94c7867919d4
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-07-06 09:06:35 -0400

    Update youtube-black-background.user.js
    Improve in playlists

--- theme/youtube-black-background.user.js
+++ theme/youtube-black-background.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           YouTube Black Background
 // @namespace      https://github.com/AbdurazaaqMohammed
-// @version        1.5.3
+// @version        1.5.4
 // @author         Abdurazaaq Mohammed
 // @description    Change background color to AMOLED black on YouTube
 // @match          https://www.youtube.com/*
@@ -14,6 +14,6 @@
 (function() {
 	'use strict';
 
-  document.head.appendChild(document.createElement('style')).innerHTML = 'div:not(button div):not(#container div):not(#dismissible div):not(#shorts-container div):not(.ytd-backstage-items.style-scope div):not(#contentWrapper div):not(#video-preview div):not(li div):not(#items *), yt-spec-touch-feedback-shape__fill div, #contentWrapper *:not(tp-yt-paper-item):not(tp-yt-paper-item *), #sections, #ytp-id-18, .ytd-rich-metadata-row-renderer * { background-color: black !important; }';
+  document.head.appendChild(document.createElement('style')).innerHTML = 'div:not(button div):not(#container div):not(#dismissible div):not(#shorts-container div):not(.ytd-backstage-items.style-scope div):not(#contentWrapper div):not(#video-preview div):not(li div):not(#items *), ytd-playlist-panel-renderer div:not(ytd-playlist-panel-video-renderer div), yt-spec-touch-feedback-shape__fill div, #contentWrapper *:not(tp-yt-paper-item):not(tp-yt-paper-item *), #sections, #ytp-id-18, .ytd-rich-metadata-row-renderer *, #primary { background-color: black !important; }';
 
 })();
