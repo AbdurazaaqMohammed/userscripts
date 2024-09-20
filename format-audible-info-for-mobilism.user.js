1756234b39a4ecb17753176f6bf6aa18cbd2e657
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-09-20 19:26:01 -0400

    Update format-audible-info-for-mobilism.user.js
    fix break command in wrong place
    add Audiobooks > Fiction or Audiobooks > Non-Fiction before the category

--- format-audible-info-for-mobilism.user.js
+++ format-audible-info-for-mobilism.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name            Copy Audible book info for Mobilism
-// @namespace       https://github.com/AbdurazaaqMohammed
-// @version         1.0
-// @author          Abdurazaaq Mohammed
-// @description     Format Audible book information for posting on Mobilism and copy to clipboard
-// @match           https://www.audible.com/pd/*
-// @match           https://images.mobilism.org/*
-// @match           https://ezgif.com/*
-// @license         The Unlicense
-// @homepage        https://github.com/AbdurazaaqMohammed/userscripts
-// @supportURL      https://github.com/AbdurazaaqMohammed/userscripts/issues
-// @grant           GM_setValue
-// @grant           GM_getValue
-// @grant           GM_setClipboard
-// @grant           GM_openInTab
+// @name		    Copy Audible book info for Mobilism
+// @namespace	  https://github.com/AbdurazaaqMohammed
+// @version		  1.0.1
+// @author		  Abdurazaaq Mohammed
+// @description	Format Audible book information for posting on Mobilism and copy to clipboard
+// @match		    https://www.audible.com/pd/*
+// @match       https://images.mobilism.org/*
+// @match       https://ezgif.com/*
+// @homepage	  https://github.com/AbdurazaaqMohammed/userscripts
+// @license		  The Unlicense
+// @supportURL	https://github.com/AbdurazaaqMohammed/userscripts/issues
+// @grant		    GM_setValue
+// @grant		    GM_getValue
+// @grant		    GM_setClipboard
+// @grant		    GM_openInTab
 // ==/UserScript==
 
 (function() {
@@ -27,9 +27,10 @@
         const w = image.naturalWidth;
         checkImageSize();
         if (needsResize(h, w)) {
-            setNewRes(h, w, h / w);
+            setNewRes(h/w);
             openTab('https://ezgif.com/resize?url=' + image);
-        } else if (GM_getValue('needsCompress')) {
+        }
+        else if (GM_getValue('needsCompress')) {
             openTab('https://ezgif.com/optimize?url=' + image);
         } else {
             GM_setValue('u', image);
@@ -38,17 +39,20 @@
     }
 
     function getValue(key) {
-        return new Promise((resolve) => {
-            GM_getValue(key, (value) => {
+        return new Promise((resolve, reject) => {
+            try {
+                const value = GM_getValue(key);
                 resolve(value);
-            });
+            } catch (error) {
+                reject(error);
+            }
         });
     }
 
     function openTab(link) {
         try {
             GM_openInTab(link);
-        } catch (e) {
+        } catch(e) {
             window.open(link, '_blank');
         }
     }
@@ -59,40 +63,53 @@
     }
 
     function copyImageLink() {
-        GM_setClipboard(GM_getValue("formattedInfo") + '\n\n' + document.querySelector('#codelbb').value + '[break]\n[u]Download Instructions:[/u]');
-        alert('Audiobook information has been copied to clipboard!');
+        const link = document.querySelector('#codelbb');
+        GM_setValue('i', link.value);
     }
 
     function checkImageSize() {
         fetch(url)
-            .then(response => {
-                if (response.ok) {
-                    return response;
-                }
-                throw new Error('Network response was not ok.');
-            })
-            .then(response => {
-                // Access the Content-Length header to get the file size
-                const contentLength = response.headers.get('Content-Length');
-                if (contentLength > 4000000) GM_setValue('needsCompress', true);
-            })
-            .catch(error => console.error('Error fetching image:', error));
+        .then(response => {
+            if (response.ok) {
+                return response;
+            }
+            throw new Error('Network response was not ok.');
+        })
+        .then(response => {
+            const contentLength = response.headers.get('Content-Length');
+            if (contentLength > 4000000) GM_setValue('needsCompress', true);
+        })
+        .catch(error => console.error('Error fetching image:', error));
     }
 
     function needsResize(h, w) {
         return h > 2499 || w > 2499;
     }
 
-    function setNewRes(height, width, aspectRatio) {
-        if (height > width) {
-            height = 2499;
-            width = Math.round(height / aspectRatio);
-        } else {
+    function setNewRes(aspectRatio) {
+        let height, width;
+        if (aspectRatio > 1) {
             width = 2499;
-            height = Math.round(width * aspectRatio);
+            height = Math.round(width / aspectRatio);
+        } else {
+            height = 2499;
+            width = Math.round(height * aspectRatio);
         }
         GM_setValue('newHeight', height);
         GM_setValue('newWidth', width);
+    }
+
+    // List of Fiction categories
+    const fictionCategories = [
+        "Classics", "Fantasy", "Science Fiction", "Historical", "Horror", "Humor",
+        "LGBTQ+", "Mystery", "Thriller & Suspense", "Romantic Comedy", "Romantic Suspense",
+        "Western", "Paranormal", "Action & Adventure", "Drama & Plays", "Anthologies & Short Stories",
+        "Genre Fiction"
+    ];
+
+    function isFiction(category) {
+        // Check if the category matches any of the Fiction categories
+        return fictionCategories.some(fiction => category.includes(fiction));
     }
 
     function formatBookInfo() {
@@ -110,25 +127,34 @@
         const categoryElement = document.querySelector('.bc-chip');
         const category = categoryElement ? categoryElement.innerText : 'Unknown Category';
 
+        // Determine Fiction or Non-Fiction category
+        const genrePrefix = isFiction(category) ? "Fiction" : "Non-Fiction";
         let formattedInfo = `[b]${title} by ${author} Narrated by ${narrator}[/b]
 [u]Requirements:[/u] .M4A/.M4B reader, ${fileSize} MB ${runtime}
 [u]Overview:[/u] ${summary}
-[b]Genre:[/b] Audiobooks > ${category}`;
-        GM_setValue("formattedInfo", formattedInfo);
+[b]Genre:[/b] Audiobooks > ${genrePrefix} > ${category}`;
+
         goToMobilism(document.querySelector("#center-1 > div > div.hero-content.bc-pub-clearfix.bc-container.hero-overflow-visible > div > div > div > div.bc-col-responsive.bc-col-3 > div > div:nth-child(1) > img").src);
+        getValue('i').then(value => {
+            formattedInfo += '\n\n' + value + '\n[break]\n[u]Download Instructions:[/u]';
+            GM_setClipboard(formattedInfo);
+        }).catch(error => {
+            GM_setClipboard(formattedInfo);
+            console.error(error);
+        });
+
+        alert('Audiobook information has been copied to clipboard!');
     }
 
-    if (url.endsWith(mobilism)) {
-        window.addEventListener('load', uploadImg);
-    } else if (url.endsWith('.php')) {
-        window.addEventListener('load', copyImageLink);
-    } else if (url.startsWith('https://ezgif.com/resize')) {
+    if (url.endsWith(mobilism)) window.addEventListener('load', uploadImg);
+    else if (url.endsWith('.php')) window.addEventListener('load', copyImageLink);
+    else if (url.startsWith('https://ezgif.com/resize')) {
         document.querySelector(".new-height.number.text").value = GM_getValue('newHeight');
         document.querySelector(".new-width.number.text").value = GM_getValue('newWidth');
         document.getElementById('ar').selectedIndex = 2;
-    } else if (url.includes('ezgif.com/tmp/')) {
-        goToMobilism();
-    } else {
+    }
+    else if (url.includes('ezgif.com/tmp/')) goToMobilism();
+    else {
         const button = document.createElement('button');
         button.innerText = 'Copy Formatted Info';
         button.style.position = 'fixed';
@@ -139,4 +165,5 @@
         document.body.appendChild(button);
         GM_deleteValue('i');
     }
+
 })();
