7aafea3bfc6cc580398e9ef512a576b74f55ab77
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-11-06 15:23:14 -0400

    Update youtube-black-background.user.js
    fix thumbnail not visible
    fix text visibility on firefox

--- theme/youtube-black-background.user.js
+++ theme/youtube-black-background.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		   YouTube Black Background
 // @namespace	  https://github.com/AbdurazaaqMohammed
-// @version		1.7
+// @version		1.7.2
 // @author		 Abdurazaaq Mohammed
 // @description	Change background color to AMOLED black on YouTube
 // @match		  https://www.youtube.com/*
@@ -13,30 +13,14 @@
 // ==/UserScript==
 (function() {
 	'use strict';
-	const myCSS = 'div:not(button div):not(ytd-shorts div):not(#container div):not(#dismissible div):not(.ytd-backstage-items.style-scope div):not(#contentWrapper div):not(#owner div):not(#video-preview div):not(li div):not(#items *):not(#page-manager > ytd-browse > ytd-playlist-header-renderer div), ytd-playlist-panel-renderer div:not(ytd-playlist-panel-video-renderer div), yt-spec-touch-feedback-shape__fill div, #contentWrapper *:not(tp-yt-paper-item):not(tp-yt-paper-item *), #sections, #ytp-id-18, .ytd-rich-metadata-row-renderer *, body > ytd-app > ytd-popup-container > tp-yt-paper-dialog > ytd-voice-search-dialog-renderer, #backstage-post-dialog > ytd-backstage-post-dialog-renderer > div, body > ytd-app > ytd-popup-container > tp-yt-paper-dialog, #header-author *, #content > ytd-mini-guide-renderer, #endpoint, #dismissible div:not(button div):not(#thumbnail div), #header, #page-manager > ytd-shorts > div.navigation-container.style-scope.ytd-shorts { background-color: black !important; color: white !important; }';
+	const myCSS = '* { color: white !important; } ytd-app, div:not(button div):not(ytd-shorts div):not(#container div):not(#dismissible div):not(.ytd-backstage-items.style-scope div):not(#contentWrapper div):not(#owner div):not(#video-preview div):not(li div):not(#items *):not(#page-manager > ytd-browse > ytd-playlist-header-renderer div), ytd-playlist-panel-renderer div:not(ytd-playlist-panel-video-renderer div), yt-spec-touch-feedback-shape__fill div, #contentWrapper *:not(tp-yt-paper-item):not(tp-yt-paper-item *), #sections, #ytp-id-18, .ytd-rich-metadata-row-renderer *, body > ytd-app > ytd-popup-container > tp-yt-paper-dialog > ytd-voice-search-dialog-renderer, #backstage-post-dialog > ytd-backstage-post-dialog-renderer > div, body > ytd-app > ytd-popup-container > tp-yt-paper-dialog, #header-author *, #content > ytd-mini-guide-renderer, #endpoint, #dismissible div:not(button div):not(#thumbnail div):not(.yt-spec-avatar-shape__image-overlays):not(.yt-spec-avatar-shape__image), #header, #page-manager > ytd-shorts > div.navigation-container.style-scope.ytd-shorts, #teaser-carousel > yt-video-metadata-carousel-view-model { background-color: black !important; };';
 
-	 if (typeof GM_addStyle != "undefined") {
-		GM_addStyle(myCSS);
-	} else if (typeof PRO_addStyle != "undefined") {
-		PRO_addStyle(myCSS);
-	} else if (typeof addStyle != "undefined") {
-		addStyle(myCSS);
-	} else {
-		var node = document.createElement("style");
-		node.type = "text/css";
-		node.appendChild(document.createTextNode(myCSS));
+	if (typeof GM_addStyle != "undefined") GM_addStyle(myCSS);
+  else if (typeof PRO_addStyle != "undefined") PRO_addStyle(myCSS);
+  else if (typeof addStyle != "undefined") addStyle(myCSS);
+	else {
+		var node = document.createElement("style"); node.type = "text/css"; node.appendChild(document.createTextNode(myCSS));
 		var heads = document.getElementsByTagName("head");
-		if (heads.length > 0) {
-			heads[0].appendChild(node);
-		} else {
-			if (document.documentElement) document.documentElement.appendChild(node);
-			else {
-				// work-around for https://github.com/greasemonkey/greasemonkey/issues/2996
-				var obs = new MutationObserver(function () {
-					if (document.documentElement) { obs.disconnect(); document.documentElement.appendChild(node); }
-				});
-				obs.observe(document, {childList: true});
-			}
-		}
+		heads.length > 0 ? heads[0].appendChild(node) ? (document.documentElement) : document.documentElement.appendChild(node) : new MutationObserver(function () { if (document.documentElement) { obs.disconnect(); document.documentElement.appendChild(node); } }).observe(document, {childList: true});
 	}
 })();
