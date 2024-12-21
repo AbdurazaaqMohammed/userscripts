570e54a76ba4746d394831bcc817b184ae302224
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-12-21 15:02:21 +0000

    Update youtube-black-background.user.js
    simple ting

--- theme/youtube-black-background.user.js
+++ theme/youtube-black-background.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		   YouTube Black Background
 // @namespace	  https://github.com/AbdurazaaqMohammed
-// @version		1.7.2.3
+// @version		1.7.2.4
 // @author		 Abdurazaaq Mohammed
 // @description	Change background color to AMOLED black on YouTube
 // @match		  https://www.youtube.com/*
@@ -9,18 +9,9 @@
 // @license		The Unlicense
 // @supportURL	 https://github.com/AbdurazaaqMohammed/userscripts/issues
 // @run-at		 document-start
-// @grant		  GM_addStyle
+// @grant		  none
 // ==/UserScript==
 (function() {
 	'use strict';
-	const myCSS = '* { color: white !important; } yt-searchbox *, #masthead > div:nth-child(5), ytd-app, div:not(.lock-scrollbar div):not(yt-lockup-view-model div):not(button div):not(#page-header > yt-page-header-renderer > yt-page-header-view-model > div > div.page-header-view-model-wiz__page-header-headline > yt-decorated-avatar-view-model > yt-avatar-shape div):not(ytd-shorts div):not(#container div):not(#dismissible div):not(.ytd-backstage-items.style-scope div):not(#contentWrapper div):not(#owner div):not(#video-preview div):not(li div):not(#items *):not(#page-manager > ytd-browse > ytd-playlist-header-renderer div), ytd-playlist-panel-renderer div:not(ytd-playlist-panel-video-renderer div), yt-spec-touch-feedback-shape__fill div, #contentWrapper *:not(tp-yt-paper-item):not(tp-yt-paper-item *), #sections, #ytp-id-18, .ytd-rich-metadata-row-renderer *, body > ytd-app > ytd-popup-container > tp-yt-paper-dialog > ytd-voice-search-dialog-renderer, #backstage-post-dialog > ytd-backstage-post-dialog-renderer > div, body > ytd-app > ytd-popup-container > tp-yt-paper-dialog, #header-author *, #content > ytd-mini-guide-renderer, #endpoint, #dismissible div:not(yt-horizontal-list-renderer div):not(button div):not(#thumbnail div):not(.yt-spec-avatar-shape__image-overlays):not(.yt-spec-avatar-shape__image), #header, #page-manager > ytd-shorts > div.navigation-container.style-scope.ytd-shorts, #teaser-carousel > yt-video-metadata-carousel-view-model, yt-chip-cloud-chip-renderer, #footer { background-color: black !important; } #right-arrow::before, #left-arrow::after { content: none !important; }';
-
-	if (typeof GM_addStyle != "undefined") GM_addStyle(myCSS);
-  else if (typeof PRO_addStyle != "undefined") PRO_addStyle(myCSS);
-  else if (typeof addStyle != "undefined") addStyle(myCSS);
-	else {
-		var node = document.createElement("style"); node.type = "text/css"; node.appendChild(document.createTextNode(myCSS));
-		var heads = document.getElementsByTagName("head");
-		heads.length > 0 ? heads[0].appendChild(node) ? (document.documentElement) : document.documentElement.appendChild(node) : new MutationObserver(function () { if (document.documentElement) { obs.disconnect(); document.documentElement.appendChild(node); } }).observe(document, {childList: true});
-	}
+	(document.head || document.documentElement).appendChild(document.createElement('style')).textContent = '* { color: white !important; } yt-searchbox *, #masthead > div:nth-child(5), ytd-app, div:not(.lock-scrollbar div):not(yt-lockup-view-model div):not(button div):not(#page-header > yt-page-header-renderer > yt-page-header-view-model > div > div.page-header-view-model-wiz__page-header-headline > yt-decorated-avatar-view-model > yt-avatar-shape div):not(ytd-shorts div):not(#container div):not(#dismissible div):not(.ytd-backstage-items.style-scope div):not(#contentWrapper div):not(#owner div):not(#video-preview div):not(li div):not(#items *):not(#page-manager > ytd-browse > ytd-playlist-header-renderer div), ytd-playlist-panel-renderer div:not(ytd-playlist-panel-video-renderer div), yt-spec-touch-feedback-shape__fill div, #contentWrapper *:not(tp-yt-paper-item):not(tp-yt-paper-item *), #sections, #ytp-id-18, .ytd-rich-metadata-row-renderer *, body > ytd-app > ytd-popup-container > tp-yt-paper-dialog > ytd-voice-search-dialog-renderer, #backstage-post-dialog > ytd-backstage-post-dialog-renderer > div, body > ytd-app > ytd-popup-container > tp-yt-paper-dialog, #header-author *, #content > ytd-mini-guide-renderer, #endpoint, #dismissible div:not(yt-horizontal-list-renderer div):not(button div):not(#thumbnail div):not(.yt-spec-avatar-shape__image-overlays):not(.yt-spec-avatar-shape__image), #header, #page-manager > ytd-shorts > div.navigation-container.style-scope.ytd-shorts, #teaser-carousel > yt-video-metadata-carousel-view-model, yt-chip-cloud-chip-renderer, #footer { background-color: black !important; } #right-arrow::before, #left-arrow::after { content: none !important; }';
 })();
