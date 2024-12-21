90aea39c5ee5acbffd9e1f4e8dde2908fc60fcba
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-12-21 15:14:47 +0000

    Update old-reddit-black-background.user.js
    ensure on firefox

--- reddit/old-reddit-black-background.user.js
+++ reddit/old-reddit-black-background.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Old Reddit Black Background
 // @namespace      https://github.com/AbdurazaaqMohammed
-// @version        1.1.2
+// @version        1.1.3
 // @author         Abdurazaaq Mohammed
 // @description    Change background color to AMOLED black on Old Reddit
 // @match          https://old.reddit.com/*
@@ -15,5 +15,5 @@
 (function() {
 	'use strict';
 
-	document.head.appendChild(document.createElement('style')).innerHTML = 'div:not(button div):not(.reddit-video-player-root.portrait div) { background-color: black !important; }';
+	(document.head || document.documentElement).appendChild(document.createElement('style')).textContent = 'div:not(button div):not(.reddit-video-player-root.portrait div) { background-color: black !important; }';
 })();
