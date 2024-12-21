248acd6815b18d91d1911e9ad0f90568ad6e5921
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-12-21 15:41:26 +0000

    Update remove-bing-related-searches.user.js
    simple ting

--- remove-bing-related-searches.user.js
+++ remove-bing-related-searches.user.js
@@ -2,26 +2,17 @@
 // @name         Remove Bing Related Searches
 // @namespace    https://github.com/AbdurazaaqMohammed
 // @homepage     https://github.com/AbdurazaaqMohammed/userscripts
-// @version      1.1
+// @version      1.1.1
 // @description  Remove related searches from the sidebar of Bing search pages
 // @author       Abdurazaaq Mohammed
 // @match        https://*.bing.com/*
 // @run-at       document-start
-// @grant        GM_addStyle
+// @grant        none
 // @license      The Unlicense
 // @supportURL   https://github.com/AbdurazaaqMohammed/userscripts/issues
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    const myCSS = '.richrswrapper { display: none; }';
-
-    if (typeof GM_addStyle != "undefined") GM_addStyle(myCSS);
-    else if (typeof PRO_addStyle != "undefined") PRO_addStyle(myCSS);
-    else if (typeof addStyle != "undefined") addStyle(myCSS);
-    else {
-		var node = document.createElement("style"); node.type = "text/css"; node.appendChild(document.createTextNode(myCSS));
-		var heads = document.getElementsByTagName("head");
-		heads.length > 0 ? heads[0].appendChild(node) ? (document.documentElement) : document.documentElement.appendChild(node) : new MutationObserver(function () { if (document.documentElement) { obs.disconnect(); document.documentElement.appendChild(node); } }).observe(document, {childList: true});
-	}
+  'use strict';
+  (document.head || document.documentElement).appendChild(document.createElement('style')).textContent = '.richrswrapper { display: none; }';
 })();
