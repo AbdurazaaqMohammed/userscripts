69358a8c6949110444e2b8c527a61d6095206fae
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-12-25 00:14:26 +0000

    Update reddit-adblock.user.js
    remove an add in mobile new reddit comments

--- reddit/reddit-adblock.user.js
+++ reddit/reddit-adblock.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit AdBlock
 // @namespace    https://github.com/AbdurazaaqMohammed
-// @version      1.0.1
+// @version      1.0.3
 // @description  Block ads on Reddit (both new and old)
 // @author       Abdurazaaq Mohammed
 // @homepage     https://github.com/AbdurazaaqMohammed/userscripts
@@ -12,5 +12,5 @@
 // @run-at       document-start
 // ==/UserScript==
 (function() {
-  (document.head || document.documentElement).appendChild(document.createElement('style')).textContent = 'shreddit-ad-post, shreddit-dynamic-ad-link, shreddit-comments-page-ad, div[class*="promotedlink"] { display: none !important; }';
+  (document.head || document.documentElement).appendChild(document.createElement('style')).textContent = 'shreddit-ad-post, shreddit-dynamic-ad-link, shreddit-comments-page-ad, shreddit-comment-tree-ad, div[class*="promotedlink"] { display: none !important; }';
 })();
